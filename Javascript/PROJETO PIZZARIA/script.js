8a627a9b1b0f41089b73ea9b14fff813649e1bdc
VictorHCamargo <victor.h0786@gmail.com>
2025-05-29 23:51:59 -0300

    Pizzaria - Cadastro finalizado

--- Javascript/PROJETO PIZZARIA/script.js
+++ Javascript/PROJETO PIZZARIA/script.js
@@ -1,18 +1,38 @@
 
 //Armazena os dados do cliente
-function exibirMensagem(mensagem,tipo){
-    const texto = document.getElementById("resultado");
-    texto.textContent = mensagem;
-    texto.className = `resultado_${tipo}`;
+function exibirMensagem(local,mensagem,tipo){
+    const posicao = document.getElementById(local);
+    const texto = document.createElement("p");
+    texto.innerHTML = `${mensagem}`;
+    texto.className = `${tipo}`;
     texto.classList.remove("hidden");
+    posicao.appendChild(texto)
     setTimeout(() => {
         texto.classList.add("hidden");
     }, 3000);
 }
-let dados = {};
-let pizza = [];
+//const clientes = [];
 function cadastrar() {
-    
+    const usuario = document.getElementById("usuario_cadastro").value;
+    const cep = parseInt(document.getElementById("cep_cadastro").value);
+    const senha = document.getElementById("senha_cadastro").value;
+    const confir = document.getElementById("confir_senha_cadastro").value;
+    if (usuario && cep && senha && confir) {
+        if (senha == confir) {
+            let clientes = JSON.parse(localStorage.getItem("clientes")) || [];
+            clientes.push({usuario,cep,senha,confir});
+            localStorage.setItem("clientes", JSON.stringify(clientes))
+            exibirMensagem("cadastro","Cadastro realizado com sucesso","sucesso");
+            document.getElementById("usuario_cadastro").value = "";
+            document.getElementById("cep_cadastro").value = "";
+            document.getElementById("senha_cadastro").value = "";
+            document.getElementById("confir_senha_cadastro").value = "";
+        } else {
+            exibirMensagem("cadastro","A senha e confirmar senha tem que ser igual","erro");
+        }
+    } else {
+        exibirMensagem("cadastro","Preencha todos os campos corretamente","erro");
+    }
 }
 function validarLogin() {
     const nome = document.getElementById("usuario").value;
@@ -20,14 +40,29 @@ function validarLogin() {
     const nomeConfirma = "admin";
     const senhaConfirma = "1234";
     if (nome === nomeConfirma && senha === senhaConfirma) {
-        exibirMensagem("Logado com sucesso!","sucesso");
+        exibirMensagem("login","Logado como funcionário!","sucesso");
         setTimeout(() => {
             window.location.href = "index.html";
         }, 1000);
     } else {
-        exibirMensagem("Usuário ou senha estão incorretos.","erro");
+        let clientes = JSON.parse(localStorage.getItem("clientes")) || [];
+        // Verifica se existe um cliente com o nome e senha informados
+        const clienteEncontrado = clientes.find(cliente => cliente.usuario === nome && cliente.senha === senha);
+        if (nome !== "" && senha !== "") {
+            if (clienteEncontrado) {
+                exibirMensagem("login","login realizado com sucesso","sucesso");
+                setTimeout(() => {
+                    window.location.href = "cliente.html";
+                }, 1000);
+            } else {
+                exibirMensagem("login","nome ou senhas errados","erro");
+            }
+        } else {
+            exibirMensagem("login","Preencha os campos corretamente","erro");
+        }
     }
 }
+let pizza = [];
 function mostrarSecaoFun(secao) {
      //Esconde as seções
     document.getElementById(`cadastroPizza`).classList.add("hidden");
@@ -46,9 +81,9 @@ function cadastroPizza() {
         document.getElementById("ingridientes").value = "";
         document.getElementById("preco").value = "";
         atualizarLista();
-        exibirMensagem("Cadastro foi concluído com sucesso","sucesso")
+        exibirMensagem("cadastroPizza","Cadastro foi concluído com sucesso","sucesso")
     } else {
-        exibirMensagem("Insira todos os campos corretamente, por favor","erro");
+        exibirMensagem("cadastroPizza","Insira todos os campos corretamente, por favor","erro");
     }
 }
 function consultar() {
